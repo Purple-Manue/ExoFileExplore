550594f9c343e29c93fcf89710909459531e0863
Silv3rgr3g <silver.gr3g@gmail.com>
2018-01-26 09:42:40 +0100

    push initial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="librairies/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+
+    <script type="text/javascript" src="librairies/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+  </body>
+
+</html>
