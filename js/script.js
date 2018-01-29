c26bd1ea21e1f1d281790c7ad65b5338f273afac
Silv3rgr3g <silver.gr3g@gmail.com>
2018-01-29 11:35:12 +0100

    ajout des icones

--- js/script.js
+++ js/script.js
@@ -11,8 +11,16 @@ $(document).ready(function() { //dès que le DOM est chargé
       dataType: 'json', // on déclare que les données seront un tableau json
       success: function(json) {
        for(var key in json) { // on lance une boucle dans le tableau
-         $('#dossiers').append('<span class="file">' + json[key] + '<br />' + '</span>');
-       } // pour chaque élément dans le tableau, on créée un span contenant la classe file avec le contenu
+         var name = json[key].indexOf(".");
+          if (name != "-1") {
+            $('#dossiers').append('<span class="file">' + '<i class="fas fa-file fa-2x">' + '</i>' + json[key] + '</span>' + '<br />');
+            //console.log("coucou");
+          } else {
+            $('#dossiers').append('<span class="file">' + '<i class="fas fa-folder-open fa-2x">' + '</i>' + json[key] + '</span>' + '<br />');
+            //console.log("pascoucou");
+          }
+       }
+
          $('.file').click(function(){
            var rep = $(this).text(); // on récupère le contenu du span
            console.log(rep);
