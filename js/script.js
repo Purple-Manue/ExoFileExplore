c5dfba33bc7a546ffd9c46cb105b49a0e33c83ab
Silv3rgr3g <silver.gr3g@gmail.com>
2018-01-30 15:13:39 +0100

    JSON ta mère!

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
 $(document).ready(function() { //dès que le DOM est chargé
-  function explore() {
-    var dir = 'current'; // j'initialise une variable dir
-    $.post('fonctions.php', {dir}, function(data, status) { //j'envoie la variable dir à fonctions.php
-      $('#chemin').html(data); //Affiche moi dans la span avec id="chemin" le retour de fonctions.php
+
+  function explore(repertoire) {
+    var dir = repertoire; // j'initialise une variable dir
+    //console.log(repertoire);
+    $.post('fonctions.php', {repertoire: dir}, function(data, status) { //j'envoie la variable dir à fonctions.php
+      $('#chemin').html(dir); //Affiche moi dans la span avec id="chemin" le retour de fonctions.php
     });
+    //console.log(dir);
 
     $.ajax({
       type:"POST", // on déclare un type post pour la requête
       url: 'fonctions.php', // on cible le fichier qui contient les données
-      dataType: 'json', // on déclare que les données seront un tableau json
+      data: "repertoire="+dir,
+      //dataType: 'json', // on déclare que les données seront un tableau json
       success: function(json) {
-       for(var key in json) { // on lance une boucle dans le tableau
-         var name = json[key].indexOf("."); // on initialise la variable name dans laquelle on regarde pour chaque clé si elle contient un point
+      //  console.log(json);
+
+      var lst = JSON.parse(json);
+      console.log(lst);
+       for (var i=0; i<lst[1].length; i++) { // on lance une boucle dans le tableau
+         var name = lst[1][i].indexOf("."); // on initialise la variable name dans laquelle on regarde pour chaque clé si elle contient un point
           if (name != "-1") { // si il y a un point dans le nom, on considère qu'il s'agit d'un fichier
-            $('#dossiers').append('<span class="file col-6 col-md-3">' + '<i class="fas fa-file fa-4x">' + '</i>' + '<br />' + json[key] + '</span>');
+            $('#dossiers').append('<span class="file col-6 col-md-3">' + '<i class="fas fa-file fa-4x">' + '</i>' + '<br />' + lst[1][i] + '</span>');
           } else { // sinon, on considère que c'est un dossier et on affiche une icone en conséquence
-            $('#dossiers').append('<span class="file col-6 col-md-3">' + '<i class="fas fa-folder-open fa-4x">' + '</i>' + '<br />' + json[key] + '</span>');
+            $('#dossiers').append('<span class="file col-6 col-md-3">' + '<i class="fas fa-folder-open fa-4x">' + '</i>' + '<br />' + lst[1][i] + '</span>');
           }
        }
 
-         $('.file').click(function(){
-           var rep = $(this).text(); // on récupère le contenu du span
-           //console.log(rep);
-           //explore();
-         });
-      }
+       $('.file').click(function(){
+         var rep = $(this).text(); // on récupère le contenu du span
+         //console.log(rep);
+         $.post ('fonctions.php', {repertoire: rep}, function(data, status) {
+          $('#dossiers').html(data);
+            explore(rep);
+         })
+       });
+
+     }
+
     });
   }
 
-  explore();
-  //alert("coucou");
-
+  explore("/home");
 
 });
