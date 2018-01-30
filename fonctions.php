6eb8146c08e9a7ed1dafb2b07fdb8dfa703c8833
Silv3rgr3g <silver.gr3g@gmail.com>
2018-01-30 16:54:25 +0100

    un peu d'avancées

--- fonctions.php
+++ fonctions.php
@@ -3,26 +3,32 @@
   function repertoire($dir) {
     $current = shell_exec("cd $dir; pwd"); // récupère le répertoire courant
     //$current = json_encode($current);
-    return $current; // affiche le répertoire courant
+    return rtrim($current); // affiche le répertoire courant
   }
 
   function dossiers($dir) {
     //if (isset($dir)) {
       $files = shell_exec("cd $dir; ls -m"); // liste les fichiers et dossiers du répertoire courant dans une chaine de caractères
-      $file = explode(",", $files); // on transforme la chaîne de caractères en tableau
+      $file = explode(", ", rtrim($files)); // on transforme la chaîne de caractères en tableau
       //$result = json_encode($file);
       return $file; // affiche la liste au format json
     //}
   }
 
   function retour() {
-    $back = shell_exec("cd ../"); // retourne au répertoire précédent
+    shell_exec("cd ../"); // retourne au répertoire précédent
+    $result = shell_exec("pwd");
+    return rtrim($result);
   }
 
   if (isset($_POST['repertoire'])) {
     $res[0] = repertoire($_POST['repertoire']);
-    $res[1] = dossiers($_POST['repertoire']); //j'exécute la fonction dossiers
+    $res[1] = dossiers($_POST['repertoire']);
     echo json_encode($res);
+  }
+
+  if ($_POST['action'] == "retour") {
+    retour();
   }
 
 ?>
